3a41eb689b7c8daeb2bd363e3dc960b42494c150
abigael mwelu <abigael.mutuku@student.moringaschool.com>
2023-07-10 09:02:41 +0300

    almost completed assignment

--- js/index.js
+++ js/index.js
@@ -1,34 +1,59 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.getElementById("menu")
 
+fetch('db.json')
+  .then(response => response.json())
+  .then(data => {
+    const menuItemsContainer = document.getElementById('menuItems');
+    const orderList = document.getElementById('orderList');
+    const menu = data.menu;
+    const orders = data.orders;
 
-    fetch("http://localhost:3000/menu")
-        .then(res => res.json())
-        .then(menu => {
+    menu.forEach(item => {
+      const menuItem = document.createElement('div');
+      menuItem.classList.add('menu-item');
 
+      const image = document.createElement('img');
+      image.src = item.img;
+      image.alt = item.name;
+      menuItem.appendChild(image);
 
-            menu.forEach(food => {
-                const foodElement = createFoodElement(food)
-                menu.appendChild(food)
-            })
-        })
-    function foodElement(food) {
-        const foodElement = document.createElement("div")
-        foodElement.classList.add("food")
+      const name = document.createElement('h3');
+      name.textContent = item.name;
+      menuItem.appendChild(name);
 
-        const nameElement = document.createElement("h1")
-        nameElement.textContent = food.name
-        foodElement.appendChild(nameElement)
+      const description = document.createElement('p');
+      description.textContent = item.description;
+      menuItem.appendChild(description);
 
-        const descriptionElement = document.createElement("p")
-        descriptionElement.textContent = food.description
-        foodElement.appendChild(descriptionElement)
+      const price = document.createElement('p');
+      price.textContent = 'Price: Ksh' + item.price;
+      menuItem.appendChild(price);
 
-        const priceElement = document.createElement("p")
-        priceElement.textContent = `$${food.price}`
-        foodElement.appendChild(priceElement)
-        return foodElement
+      const orderButton = document.createElement('button');
+      orderButton.textContent = 'Order';
+      orderButton.addEventListener('click', () => {
+        orders.push(item);
+        updateOrderList();
+        alert ('order placed')
+      });
+      menuItem.appendChild(orderButton);
+
+      menuItemsContainer.appendChild(menuItem);
+      const utensilsButton = document.createElement('button');
+      utensilsButton.textContent = 'Utensils';
+      utensilsButton.addEventListener('click', () => {
+        alert ('Utensils added')
+      });
+      menuItem.appendChild(utensilsButton);
+
+      menuItemsContainer.appendChild(menuItem);
+    });
+
+    function updateOrderList() {
+      orderList.innerHTML = '';
+      orders.forEach(order => {
+        const listItem = document.createElement('li');
+        listItem.textContent = order.name + " Ksh" + order.price ;
+        orderList.appendChild(listItem);
+      });
     }
-
-
-})
+  });
